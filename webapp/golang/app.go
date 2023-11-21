a871a419d8547616e86b2ac541d4e86363135808
NAKASONE Hiroshi <43738441+hiroshinakasone@users.noreply.github.com>
2023-11-22 01:17:01 +0900

    Update app

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -383,7 +383,17 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `id` DESC")
+	q := `
+	SELECT
+		posts.id, posts.user_id, posts.body, posts.mime, posts.created_at
+	FROM posts
+		JOIN users ON posts.user_id = users.id
+	WHERE
+		users.del_flg = 0
+	ORDER BY id DESC
+	LIMIT 20`
+
+	err := db.Select(&results, q)
 	if err != nil {
 		log.Print(err)
 		return
